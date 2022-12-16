8dbadc7324c4498e2f814982915db702ddd07a84
Dasher Evans <112717728+GoBigMode@users.noreply.github.com>
2022-12-16 09:40:33 -0800

    Initial Commit

---
+++ Project/DOPEcalc.py
@@ -0,0 +1,29 @@
+import turtle
+
+
+timothy = turtle.Turtle()
+# timothy.shape('circle')
+# timothy.shapesize(0.01, 0.01, 0.01)
+
+# turtle.pen(pencolor='brown', fillcolor='brown')
+turtle.begin_fill()
+timothy.forward(100)
+timothy.right(90)
+timothy.forward(100)
+timothy.right(90)
+timothy.forward(100)
+timothy.right(90)
+timothy.forward(100)
+turtle.end_fill()
+
+
+#class Tree(self,x,y):
+ #   def __init__(self,x,y):
+  #      self.x=x
+   #     self.y=y
+    #    turtle.draw(screen)
+     #   turtle.screen.lineTo(x+10, y-5)
+      #  turtle.screen.lineto(x+15,y-5)
+
+
+turtle.mainloop()
